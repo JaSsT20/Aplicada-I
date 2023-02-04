c986ae921ea976a27d84033f89a7b58f12ff0e90
Levid <jassontejada@gmail.com>
2023-02-03 21:55:28 -0400

    Migraciones

---
+++ Migrations/20230204013303_Inicial.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ocupations",
+                columns: table => new
+                {
+                    OcupationId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Salary = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ocupations", x => x.OcupationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    personID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    phoneNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    cellphoneNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    email = table.Column<string>(type: "TEXT", nullable: true),
+                    balance = table.Column<int>(type: "INTEGER", nullable: false),
+                    direction = table.Column<string>(type: "TEXT", nullable: true),
+                    dateBirth = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    occupationID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.personID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ocupations");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
