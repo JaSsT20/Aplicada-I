b05a0acf3568a69c2fd4b8885b2dcea7965420cb
Levid <jassontejada@gmail.com>
2023-02-03 23:07:24 -0400

    Agregando DbSet Loans

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -5,6 +5,8 @@ public class Context : DbContext
     public DbSet<Ocupations> Ocupations { get; set; }
     
     public DbSet<Person> Person { get; set; }
+
+    public DbSet<Loans> Loans { get; set; }
     public Context(DbContextOptions<Context> options): base(options)
     {
 
