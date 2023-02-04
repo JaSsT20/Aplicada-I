8c764382c07c766c8ac5d6101428759ac9cb946d
Levid <jassontejada@gmail.com>
2023-02-03 21:53:45 -0400

    Realizando dbSet Person

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -3,7 +3,8 @@ using Microsoft.EntityFrameworkCore;
 public class Context : DbContext
 {
     public DbSet<Ocupations> Ocupations { get; set; }
-
+    
+    public DbSet<Person> Person { get; set; }
     public Context(DbContextOptions<Context> options): base(options)
     {
 
