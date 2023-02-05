102d6a9e01ccb051b915ee489eca29e3dbacbbfa
Levid <jassontejada@gmail.com>
2023-02-05 10:22:14 -0400

    Agregando Payments y PaymentsDetail al contexto

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -7,6 +7,10 @@ public class Context : DbContext
     public DbSet<Person> Person { get; set; }
 
     public DbSet<Loans> Loans { get; set; }
+
+    public DbSet<Payments> Payments { get; set; }
+
+    public DbSet<PaymentsDetail> PaaymentsDetail { get; set; }
     public Context(DbContextOptions<Context> options): base(options)
     {
 
