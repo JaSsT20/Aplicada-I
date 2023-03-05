6f7d9014785f6de4b15cbb5d8cc747a572ec3d5d
Levid <jassontejada@gmail.com>
2023-03-04 23:09:39 -0400

    Agregando validaciones

--- Models/Payments.cs
+++ Models/Payments.cs
@@ -5,9 +5,15 @@ public class Payments
 {
     [Key]
     public int paymentID {get; set;}
-    public DateTime date { get; set; }
+
+    [Required(ErrorMessage = "La fehca es obligatoria.")]
+    public DateTime date { get; set; } = DateTime.Today;
+
+    [Range(0, int.MaxValue, ErrorMessage = "El ID persona es obligatorio.")]
     public int personID { get; set; }
     public string? concept { get; set; }
+
+    [Range(0, float.MaxValue, ErrorMessage = "La cantidad es obligatoria y debe ser mayor que cero.")]
     public float amount { get; set; }
-    public PaymentsDetail PaymentsDetail { get; set; } = new PaymentsDetail();
+    public List<PaymentsDetail> PaymentsDetail { get; set; } = new List<PaymentsDetail>();
 }
